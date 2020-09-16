6ea2c845b534aeb455297db158ef3a72338db482
chentaiyu0336 <2868510447@qq.com>
2020-09-16 18:25:01 +0800

    [cty] finish jest homework

--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,5 +1,14 @@
+import axios from "axios";
+import getUsers from "../users";
+
+jest.mock("axios");
 describe("users", () => {
   test("should get users data with mock axios get", () => {
     // TODO 13: add async test with manual mock
+    axios.get.mockResolvedValue({
+      data: { name: "Taiyu" },
+    });
+    const result = getUsers();
+    return expect(result).resolves.toEqual({ name: "Taiyu" });
   });
 });
--- src/mock.class/__test__/vaccineTest.test.js
+++ src/mock.class/__test__/vaccineTest.test.js
@@ -1,27 +1,48 @@
 import VaccineTest from "../vaccineTest";
 import Recipient from "../recipient";
-import Covid19Vaccine from "../covid19Vaccine";
+
+const mockAcceptInjection = jest.fn();
+const mockGetHasAntibodies = jest.fn();
+let vaccineTest;
 
 jest.mock("../recipient", () => {
   // mock class实现
+  return jest.fn().mockImplementation(() => {
+    return {
+      acceptInjection: mockAcceptInjection,
+      getHasAntibodies: mockGetHasAntibodies,
+    };
+  });
 });
 
 beforeEach(() => {
   // clear mock here
+  Recipient.mockClear();
+  mockAcceptInjection.mockClear();
+  mockGetHasAntibodies.mockClear();
+  vaccineTest = new VaccineTest();
 });
 
 describe("inject", () => {
   test("should recipient accept injection with vaccine", () => {
     // TODO 14: add test here
+    vaccineTest.inject();
+    expect(mockAcceptInjection).toHaveBeenCalledTimes(1);
   });
 });
 
 describe("test", () => {
   test("should get Success if recipient has antibodies", () => {
     // TODO 15: add test here
+    const expectResult = "Vaccine Test Success";
+    mockGetHasAntibodies.mockReturnValue(true);
+    expect(vaccineTest.test()).toEqual(expectResult);
   });
 
   test("should get Failed if recipient has no antibodies", () => {
     // TODO 16: add test here
+    const expectResult = "Vaccine Test Failed";
+    mockGetHasAntibodies.mockReturnValue(false);
+    expect(vaccineTest.test()).toEqual(expectResult);
   });
 });
